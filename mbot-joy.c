27102a7529c3208299c2939bcc39628ea3653d64
Lars Kellogg-Stedman <lars@oddbit.com>
2019-02-04 08:11:49 -0500

    documentation for mbot-joy

--- mbot-joy.c
+++ mbot-joy.c
@@ -1,3 +1,12 @@
+/**
+ * \file mbot-joy.c
+ *
+ * Control your mbot with a joystick.
+ *
+ * Connect a standard 5-pin arcade joystick to your mbot (pins GND, 9, 10,
+ * 11, 12) and control its movements. This code makes use of the ultrasonic
+ * sensor to avoid bumping into obstacles.
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -6,45 +15,52 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#include "serial.h"
+//! \defgroup OBSTACLE Obstacle avoidance settings
+//! @{
+#define MAX_SPEED  200      //!< Normal forward speed
+#define MED_SPEED  127      //!< Slow speed when approaching an obstacle
+#define STOP_DISTANCE 150   //!< Stop when <= this distance from an obstacle
+#define SLOW_DISTANCE 500   //!< Slow down when <= this distance from an obstacle
+//! @}
 
-#define MAX_SPEED  200
-#define MED_SPEED  127
-#define SLOW_SPEED 64
 
-#define STOP_DISTANCE 150
-#define SLOW_DISTANCE 500
+//! \defgroup PINS Pins and ports
+//! @{
+#define LEDDDR     (DDRB)           //!< DDR register for LED pin
+#define LEDPORTREG (PORTB)          //!< PORT register for LED pin
+#define LEDPINREG  (PINB)           //!< PIN register for LED pin
+#define LEDPIN     (_BV(PORTB5))    //!< LED pin number
 
-#define LEDDDR     (DDRB)
-#define LEDPORTREG (PORTB)
-#define LEDPINREG  (PINB)
-#define LEDPIN     (_BV(PORTB5))
+#define USDDR     (DDRC)            //!< DDR register for ultrasonic sensor
+#define USPORTREG (PORTC)           //!< PORT register for ultrasonic sensor
+#define USPINREG  (PINC)            //!< PIN register for ultrasonic sensor
+#define USPIN     (_BV(PORTC1))     //!< Data pin for ultrasonic sensor
 
-#define USDDR     (DDRC)
-#define USPORTREG (PORTC)
-#define USPINREG  (PINC)
-#define USPIN     (_BV(PORTC1))
-#define USPCINT   (PCINT8)
+#define MOTORDDR  (DDRD)            //!< DDR register for motors
+#define MOTORPORT (PORTD)           //!< PORT register for motors
+#define MLEFTDIR  (_BV(PORTD4))     //!< Left motor direction pin
+#define MLEFTPWM  (_BV(PORTD5))     //!< Left motor speed control
+#define MRIGHTDIR (_BV(PORTD7))     //!< Right motor direction pin
+#define MRIGHTPWM (_BV(PORTD6))     //!< Right motor speed control
+#define MLEFTOCR  (OCR0A)           //!< Left motor OCR register
+#define MRIGHTOCR (OCR0B)           //!< Right motor OCR register
 
-#define MOTORDDR  (DDRD)
-#define MOTORPORT (PORTD)
-#define MLEFTDIR  (_BV(PORTD4))
-#define MLEFTPWM  (_BV(PORTD5))
-#define MRIGHTDIR (_BV(PORTD7))
-#define MRIGHTPWM (_BV(PORTD6))
-#define MLEFTOCR  (OCR0A)
-#define MRIGHTOCR (OCR0B)
+#define JOYDDR    (DDRB)            //!< DDR register for joystick
+#define JOYPORT   (PORTB)           //!< PORT register for joystick
+#define JOYPIN    (PINB)            //!< PIN register for joystick
+#define PIN_LEFT  (_BV(PORTB1))     //!< Joystick left pin
+#define PIN_RIGHT (_BV(PORTB2))     //!< Joystick right pin
+#define PIN_DOWN  (_BV(PORTB3))     //!< Joystick down (reverse) pin
+#define PIN_UP    (_BV(PORTB4))     //!< Joystick up (forward) pin
+//! @}
 
-#define JOYDDR    (DDRB)
-#define JOYPORT   (PORTB)
-#define JOYPIN    (PINB)
-#define PIN_LEFT  (_BV(PORTB1))
-#define PIN_RIGHT (_BV(PORTB2))
-#define PIN_DOWN  (_BV(PORTB3))
-#define PIN_UP    (_BV(PORTB4))
-
+//! Describe motor direction
 typedef enum DIRECTION {STOPPED, FORWARD, REVERSE} DIRECTION;
+
+//! Describe current turning state
 typedef enum TURN {NONE, LEFT, RIGHT} TURN;
+
+//! States in ultrasonic measurement state machine
 typedef enum DISTANCE_STATE {
     PING,
     WAIT_PULSE_START,
@@ -52,6 +68,17 @@ typedef enum DISTANCE_STATE {
     INTERVAL
 } DISTANCE_STATE;
 
+/**
+ * Configure pins and pwm.
+ *
+ * Set up fast PWM mode with OCR output enabled on for the motor speed
+ * control pins, OC0A (PORTD6) and OC0B (PORTD5). The motor PWM frequency
+ * is 967Hz.  At 16Mhz with a prescaler of clk/64, this gets us
+ * 976.5625Hz, which is apparenlty "close enough" (that's
+ * `16000000 / 64 / 255`, where the final `/255` is because we are using 8-bit
+ * timers).  Setting `OCR0A` and `OCR0B` controls the duty cycle of the PWM
+ * output.
+ */
 void setup() {
     // pwm frequency is 967Hz
     // 16000000 / (64 * 256) = 976.5625 Hz
@@ -72,11 +99,27 @@ void setup() {
     LEDDDR |= LEDPIN;
 }
 
+/**
+ * Handle distance measurements with the ultrasonic sensor.
+ *
+ * The ultrasonic sensor on the mbot is a three-pin sensor: `GND`, `5V`,
+ * and a data pin that is used as both an input and an output. We send a 10us
+ * high pulse to the sensor to trigger a measurement. The sensor then sends out
+ * a series of ultrasonic pulses, and then reports the measurement by
+ * bringing the data line high.  The duration of the high pulse
+ * corresponds to the distance.
+ *
+ * In order to time the high pulse, we simply count loop iterations.
+ * This means that as you make changes to the code, you may need to
+ * adjust the `STOP_DISTANCE` and `SLOW_DISTANCE` settings.
+ */
 uint8_t measure_distance(uint16_t *distance) {
     static DISTANCE_STATE state = PING;
     static uint16_t counter     = 0;
     bool valid                  = false;
 
+    // Count main loop iterations. This is a static variable so it
+    // preserves its value between calls.
     counter++;
 
     switch (state) {
